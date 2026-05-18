4e5c09cdf4d9e68b70565f483ca0fda7e46315f2
ClockifySlack <clockifyslack@outlook.com>
2026-05-18 16:36:16 +0200

    Update route.ts

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,4 +1,6 @@
+export const dynamic = 'force-dynamic';
 import { NextRequest, NextResponse } from 'next/server';
+
 import { supabase } from '../../../../lib/supabase';
 
 export async function GET(request: NextRequest) {
